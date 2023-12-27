820daeb6291947ab330af9049fdc695b5176ddc3
Pasan Abeysekara <pasankavindaabey@gmail.com>
2023-12-27 07:58:12 +0530

    cart added

--- src/app/feature/checkout/checkout/checkout.component.ts
+++ src/app/feature/checkout/checkout/checkout.component.ts
@@ -1,12 +1,102 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, inject, Input, Output, TemplateRef, ViewEncapsulation} from '@angular/core';
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    FormsModule
+  ],
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css'
+  styleUrl: './checkout.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class CheckoutComponent {
+
+  private offcanvasService = inject(NgbOffcanvas);
+  openEnd(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { position: 'end' });
+  }
+
+  // @Input() valueProduct: number = 0;
+  // @Output() valueChangeProduct = new EventEmitter<number>();
+  // incrementProduct() {
+  //   this.valueProduct++;
+  //   this.emitValueChangeProduct();
+  // }
+  //
+  // decrementProduct() {
+  //   if (this.valueProduct > 0) {
+  //     this.valueProduct--;
+  //     this.emitValueChangeProduct();
+  //   }
+  // }
+  //
+  // private emitValueChangeProduct() {
+  //   this.valueChangeProduct.emit(this.valueProduct);
+  // }
+
+
+
+  @Input() valueProduct1: number = 0;
+  @Output() valueChangeProduct1 = new EventEmitter<number>();
+  incrementProduct1() {
+    this.valueProduct1++;
+    this.emitValueChangeProduct1();
+  }
+
+  decrementProduct1() {
+    if (this.valueProduct1 > 0) {
+      this.valueProduct1--;
+      this.emitValueChangeProduct1();
+    }
+  }
+
+  private emitValueChangeProduct1() {
+    this.valueChangeProduct1.emit(this.valueProduct1);
+  }
+
+
+  @Input() valueProduct2: number = 0;
+  @Output() valueChangeProduct2 = new EventEmitter<number>();
+  incrementProduct2() {
+    this.valueProduct2++;
+    this.emitValueChangeProduct2();
+  }
+
+  decrementProduct2() {
+    if (this.valueProduct2 > 0) {
+      this.valueProduct2--;
+      this.emitValueChangeProduct2();
+    }
+  }
+
+  private emitValueChangeProduct2() {
+    this.valueChangeProduct2.emit(this.valueProduct2);
+  }
+
+  @Input() valueProduct3: number = 0;
+  @Output() valueChangeProduct3 = new EventEmitter<number>();
+  incrementProduct3() {
+    this.valueProduct3++;
+    this.emitValueChangeProduct3();
+  }
+
+  decrementProduct3() {
+    if (this.valueProduct3 > 0) {
+      this.valueProduct3--;
+      this.emitValueChangeProduct3();
+    }
+  }
+
+  private emitValueChangeProduct3() {
+    this.valueChangeProduct3.emit(this.valueProduct3);
+  }
+
 
 }
